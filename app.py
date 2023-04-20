73849b8018dad6d94dd2df4d5226d15de3ffc756
wadevondoom <wade.neuwirth@gmail.com>
2023-04-19 23:19:57 -0700

    try this but I think i need a differnt ping package

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import os, ipinfo
-from icmplib import ping, multiping, traceroute, resolve
+from icmplib import traceroute
+from pythonping import ping as pping
 from statistics import pstdev
 
 db_conn = os.environ.get("DBCONN")
@@ -29,63 +30,59 @@ def index():
 
 def run_probe(ip_address):
     try:
-        host = ping(ip_address, count=4, interval=1, timeout=2, privileged=False)
+        host = pping(ip_address, count=4, interval=1, timeout=2)
 
-        if host.is_alive:
-            print(f"{host.address} is up!")
+        hops = traceroute(ip_address)
+        traceroute_data = []
+        last_distance = 0
 
-            hops = traceroute(ip_address)
-            traceroute_data = []
-            last_distance = 0
+        for hop in hops:
+            traceroute_data.append(
+                {
+                    "distance": float(hop.distance),
+                    "address": hop.address,
+                    "avg_rtt": float(hop.avg_rtt),
+                    "packet_loss": float(host.packet_loss),
+                    "packets_sent": float(hop.packets_sent),
+                    "packets_received": float(hop.packets_received),
+                }
+            )
+            print(
+                f"Current traceroute_data: {traceroute_data}"
+            )  # Add this line to debug
 
-            for hop in hops:
-                traceroute_data.append(
-                    {
-                        "distance": float(hop.distance),
-                        "address": hop.address,
-                        "avg_rtt": float(hop.avg_rtt),
-                        "packet_loss": float(host.packet_loss),
-                        "packets_sent": float(hop.packets_sent),
-                        "packets_received": float(hop.packets_received),
-                    }
-                )
-                print(
-                    f"Current traceroute_data: {traceroute_data}"
-                )  # Add this line to debug
+            if last_distance + 1 != hop.distance:
+                print("Some gateways are not responding")
+                print(f"{hop.distance}    {hop.address}    {hop.avg_rtt} ms")
 
-                if last_distance + 1 != hop.distance:
-                    print("Some gateways are not responding")
-                    print(f"{hop.distance}    {hop.address}    {hop.avg_rtt} ms")
+            last_distance = hop.distance
 
-                last_distance = hop.distance
+        rtt_values = [host.min_rtt, host.max_rtt, host.avg_rtt]
+        rtt_std_dev = pstdev(rtt_values) if len(rtt_values) >= 2 else 0.0
 
-            rtt_values = [host.min_rtt, host.max_rtt, host.avg_rtt]
-            rtt_std_dev = pstdev(rtt_values) if len(rtt_values) >= 2 else 0.0
+        host_data = {
+            "address": host.address,
+            "rtt_min": float(host.min_rtt),
+            "rtt_max": float(host.max_rtt),
+            "rtt_avg": float(host.avg_rtt),
+            "rtt_std_dev": float(rtt_std_dev),
+            "packet_loss": float(host.packet_loss),
+            "jitter": float(host.jitter),
+            "packets_sent": float(host.packets_sent),
+            "packets_received": float(host.packets_received),
+        }
 
-            host_data = {
-                "address": host.address,
-                "rtt_min": float(host.min_rtt),
-                "rtt_max": float(host.max_rtt),
-                "rtt_avg": float(host.avg_rtt),
-                "rtt_std_dev": float(rtt_std_dev),
-                "packet_loss": float(host.packet_loss),
-                "jitter": float(host.jitter),
-                "packets_sent": float(host.packets_sent),
-                "packets_received": float(host.packets_received),
-            }
+        probe_location = get_probe_location()
 
-            probe_location = get_probe_location()
+        probe_data = {
+            "host": host_data,
+            "traceroute": traceroute_data,
+            "probe_location": probe_location,
+        }
 
-            probe_data = {
-                "host": host_data,
-                "traceroute": traceroute_data,
-                "probe_location": probe_location,
-            }
+        probes_collection.insert_one(probe_data)
+        print(f"Probe data stored in database: {probe_data}")
 
-            probes_collection.insert_one(probe_data)
-            print(f"Probe data stored in database: {probe_data}")
-        else:
-            print(f"{host.address} is down!")
 
     except Exception as e:
         print(f"Error during probe: {e}")
