6b0748ba8d17180b7481447854c642804df2091a
wadevondoom <wade.neuwirth@gmail.com>
2023-04-19 17:53:46 -0700

    Some formatting

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import socket
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import os
@@ -34,57 +33,58 @@ def run_probe(ip_address):
 
         if host.is_alive:
             print(f"{host.address} is up!")
+
+            hops = traceroute(ip_address)
+            traceroute_data = []
+            last_distance = 0
+
+            for hop in hops:
+                print(f'{hop.distance}    {hop.address}    {hop.avg_rtt} ms')
+                traceroute_data.append(
+                    {
+                        "distance": hop.distance,
+                        "address": hop.address,
+                        "avg_rtt": hop.avg_rtt,
+                        "packet_loss": host.packet_loss,
+                        "packets_sent": hop.packets_sent,
+                        "packets_received": hop.packets_received,
+                    }
+                )
+
+                if last_distance + 1 != hop.distance:
+                    print("Some gateways are not responding")
+                    print(f"{hop.distance}    {hop.address}    {hop.avg_rtt} ms")
+
+                last_distance = hop.distance
+
+            rtt_values = [host.min_rtt, host.max_rtt, host.avg_rtt]
+            rtt_std_dev = pstdev(rtt_values) if len(rtt_values) >= 2 else 0
+
+            host_data = {
+                "address": host.address,
+                "rtt_min": host.min_rtt,
+                "rtt_max": host.max_rtt,
+                "rtt_avg": host.avg_rtt,
+                "rtt_std_dev": rtt_std_dev,
+                "packet_loss": host.packet_loss,
+                "jitter": host.jitter,
+                "packets_sent": host.packets_sent,
+                "packets_received": host.packets_received,
+            }
+
+            probe_location = get_probe_location(ip_address)
+
+            probe_data = {
+                "host": host_data,
+                "traceroute": traceroute_data,
+                "probe_location": probe_location,
+            }
+
+            probes_collection.insert_one(probe_data)
+            print(f"Probe data stored in database: {probe_data}")
         else:
             print(f"{host.address} is down!")
-
-        hops = traceroute(
-            ip_address, count=5, interval=0.05, timeout=2, first_hop=1, max_hops=30
-        )
-        traceroute_data = []
-        last_distance = 0
-
-        for hop in hops:
-            traceroute_data.append(
-                {
-                    "distance": hop.distance,
-                    "address": hop.address,
-                    "avg_rtt": hop.avg_rtt,
-                    "min_rtt": hop.min_rtt,
-                    "packets_sent": hop.packets_sent,
-                    "packets_received": hop.packets_received,
-                }
-            )
-
-            if last_distance + 1 != hop.distance:
-                print("Some gateways are not responding")
-                print(f"{hop.distance}    {hop.address}    {hop.avg_rtt} ms")
-
-            last_distance = hop.distance
-
-        rtt_values = [host.min_rtt, host.max_rtt, host.avg_rtt]
-        rtt_std_dev = pstdev(rtt_values) if len(rtt_values) >= 2 else 0
-
-        host_data = {
-            "address": host.address,
-            "rtt_min": host.min_rtt,
-            "rtt_max": host.max_rtt,
-            "rtt_avg": host.avg_rtt,
-            "rtt_std_dev": rtt_std_dev,
-            "packet_loss": host.packet_loss,
-            "jitter": host.jitter,
-        }
-
-        probe_location = get_probe_location(ip_address)
-
-        probe_data = {
-            "host": host_data,
-            "traceroute": traceroute_data,
-            "probe_location": probe_location,
-        }
-
-        probes_collection.insert_one(probe_data)
-        print(f"Probe data stored in database: {probe_data}")
-
+            
     except Exception as e:
         print(f"Error during probe: {e}")
 
