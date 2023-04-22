964d8fe05e2a6390f71b2e8d5ce0beb541c5f8c9
Wade Neuwirth <wade.neuwirth@gmail.com>
2023-04-21 21:53:38 -0700

    Typo in run file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /path/to/venv/bin/activate
+source /home/appuser/venv/bin/activate
 export PYTHONPATH=/path/to/geoquery
 exec gunicorn --workers 3 --bind unix:/path/to/gunicorn_socks/geoquery.sock wsgi:app
